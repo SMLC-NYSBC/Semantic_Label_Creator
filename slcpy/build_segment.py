ed7d00de14fc41221cd2d6b91fcbb4b426436d3f
RRobert92 <robert.kiewisz@gmail.com>
2021-07-02 14:49:11 +0200

    bugfix in interpolation_3D() for horizontally splayed out segments

--- slcpy/build_segment.py
+++ slcpy/build_segment.py
@@ -11,6 +11,8 @@
     :param array points: numpy array with points belonging to individual
         segments given by x, y, z coordinates from ImportDataFromAmira class
 
+    Bugfix: fix interpolation_3D for horizontally splayed out segments
+
     :author Robert Kiewisz
 
 """
@@ -30,41 +32,43 @@ def interpolation_1D(start, stop, max_len):
 # An interpolation method to calculated continuity between set of points
 def interpolation_3D(points):
     interpolated = np.zeros(
-        (int(max(points[0:len(points), 2]) - min(points[0:len(points), 2])), 3),
+        (0, 3),
         dtype=int
     )
-    # Interpolate Z axis
-    interpolated[0:len(interpolated), 2] = np.linspace(
-        int(min(points[0:len(points), 2])), int(max(points[0:len(points), 2])),
-        len(interpolated)
-    ).round()
 
-    len_counter = 0
-    for i in range(len(points) - 1):
-        if len_counter == 0:
-            max_len = abs(int(points[i, 2]) - int(points[i + 1, 2])) + 1
+    for i in range(len(points)):
+        if i == len(points)-1 or points[i + 1, 2] in (points[i, 2], int(points[i, 2] + 1)):
+            interpolated = np.append(interpolated,
+                                     points[i:i + 1, :3],
+                                     axis=0
+                                     )
         else:
-            max_len = abs(int(points[i, 2] + 1) - int(points[i + 1, 2])) + 1
+            max_len = int(abs(points[i, 2] - points[i+1, 2]) + 1)
+            df = np.zeros((
+                max_len,
+                3
+            ))
 
-        if i == int(len(points) - 2):
-            max_len = max_len - 1
-        len_of_seq = len_counter + max_len
+            x = interpolation_1D(points[i, 0],
+                                 points[i + 1, 0],
+                                 max_len
+                                 )
+            y = interpolation_1D(points[i, 1],
+                                 points[i + 1, 1],
+                                 max_len
+                                 )
+            z = interpolation_1D(points[i, 2],
+                                 points[i + 1, 2],
+                                 max_len
+                                 )
 
-        # Interpolate X, Y, Z axis
-        points_seq_x = interpolation_1D(
-            points[i, 0],
-            points[i + 1, 0],
-            max_len
-        )
-        points_seq_y = interpolation_1D(
-            points[i, 1],
-            points[i + 1, 1],
-            max_len
-        )
-
-        interpolated[len_counter:len_of_seq, 0] = points_seq_x
-        interpolated[len_counter:len_of_seq, 1] = points_seq_y
-
-        len_counter = len_counter + max_len
+            df[0:max_len, 0] = x
+            df[0:max_len, 1] = y
+            df[0:max_len, 2] = z
+            interpolated = np.append(interpolated,
+                                     df,
+                                     axis=0
+                                     )
 
     return interpolated
+
