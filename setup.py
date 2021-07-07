be7369f794485c0968d01a0e403e17790079b7b7
RRobert92 <robert.kiewisz@gmail.com>
2021-07-07 11:24:53 +0200

    fix issue for pypi

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author_email='robert.kiewisz@gmail.com',
     python_requires='>=3.8',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
